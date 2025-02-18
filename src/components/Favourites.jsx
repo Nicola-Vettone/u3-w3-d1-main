cb1836a9977529e6251da7bfc0d20a53ef8502a9
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-18 17:18:02 +0100

    click

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const Favourites = () => {
   const favouriteJobs = useSelector((state) => state.favourites.favourites);
   return (
     <ul>
       {favouriteJobs.map((favouriteJobs, index) => (
-        <li key={`job-${index}`}>{favouriteJobs}</li>
+        <li key={`job-${index}`}>
+          <Link>{favouriteJobs}</Link>
+        </li>
       ))}
     </ul>
     /*    console.log(favouriteJobs) */
